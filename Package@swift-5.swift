a76159feb55985b927763a17b9f101a56c107c88
Bo Liu <boliu@Bos-Mac-Studio.local>
2025-12-14 15:39:07 +0100

    support macOS v12

--- Package@swift-5.swift
+++ Package@swift-5.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Ollama",
     platforms: [
-        .macOS(.v13),
+        .macOS(.v12),
         .macCatalyst(.v13),
         .iOS(.v16),
         .watchOS(.v9),
